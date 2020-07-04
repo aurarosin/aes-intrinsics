b86c25077f20f082878ff68ee12e4a6a3546ed0c
Rosaura HG <rosshg.nw@gmail.com>
2020-07-04 02:57:48 -0500

    dev function ghash

--- include/gcm.h
+++ include/gcm.h
@@ -6,3 +6,5 @@ void gcm_encrypt(unsigned char* P, size_t p, unsigned char* A, size_t a,
 int gcm_decrypt(unsigned char* C, size_t c, unsigned char* A, size_t a,
                 unsigned char* IV, size_t iv, unsigned char* key,
                 unsigned char* T, unsigned char* out);
+void GHASH(unsigned char* X, size_t x, unsigned char* H, size_t h,
+           unsigned char* out);
--- lib/gcm.c
+++ lib/gcm.c
@@ -27,3 +27,26 @@ int gcm_decrypt(unsigned char* C, size_t c, unsigned char* A, size_t a,
   AES_128_Key_Expansion((const unsigned char*)key, key_expanded);
   AES_GCM_decrypt(C, out, A, IV, T, c, a, iv, key_expanded, ROUNDS);
 }
+
+void GHASH(unsigned char* X, size_t x, unsigned char* H, size_t h,
+           unsigned char* out) {
+  // 1. Let X 1 , X 2 , ... , X m-1 , X m denote the unique sequence of blocks
+  // such that X = X 1 || X 2 ||... || X m-1 || X m .
+  size_t m = ceill(x / 16.0);
+
+  u_int8_t Y[16 * m];
+
+  // 2.
+  memset(&Y[0], 0, 16);
+
+  // 3.
+  for (size_t i = 1; i <= m; i++) {
+    xor(&Y[16 * (i - 1)], &X[16 * (i - 1)], 16 * 8, &Y[16 * i]);
+    // Y[16 * i] = Y[16 * i]
+  }
+  // Ym[0] =
+
+  // 2. Let Y 0 be the “zero block,” 0^128 .
+  // 3. For i = 1, ..., m, let Y i = (Y i-1 ⊕ X i ) • H.
+  // 4. Return Y m .
+}
